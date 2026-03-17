946c29ac05c9690b1fa9a3cfff70001b2870780b
Evamirion <rimurutempest115@gmail.com>
2026-03-17 09:23:57 +0700

    Add files via upload

--- Tiemtruyenchu/src/chap.js
+++ Tiemtruyenchu/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('utf-8');
-        let htm = doc.select(".chapter-content").html();
+        let htm = doc.select("h2.text-center.fw-bold.mt-4.mb-3.text-success").html() + "</br>" + doc.select(".chapter-content").html();
         return Response.success(htm);
     }
     return null;
