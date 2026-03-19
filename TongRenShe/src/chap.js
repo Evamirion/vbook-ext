4030ddf65377a0ed3db01483ddc6425f9de351d6
Evamirion <rimurutempest115@gmail.com>
2026-03-19 10:28:50 +0700

    Add files via upload

--- TongRenShe/src/chap.js
+++ TongRenShe/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gb2312');
-        let htm = doc.select(".readDetail").html();
+        let htm = doc.select(".read_chapterName h1").text() + doc.select(".read_chapterDetail").html();
         return Response.success(htm);
     }
     return null;
