1a96392c425544439fb4951c533f467dc5eeec30
Evamirion <rimurutempest115@gmail.com>
2026-04-06 11:53:26 +0700

    Add files via upload

--- Tiemtruyenchu/src/detail.js
+++ Tiemtruyenchu/src/detail.js
@@ -6,7 +6,7 @@ function execute(url) {
         //let author =  doc.select(".mb-3.d-flex.flex-wrap.gap-2.align-items-center a").get(0).text();
         let detail = doc.select(".row.g-3.small .col-md-12.mt-3.d-flex.flex-wrap").text();
         //detail = Html.clean(detail, ["p"]);
-        let description = doc.select(".content-text").html().split("<br>")[0].replace(/ 　　/g,"<br>");
+        let description = doc.select(".content-text").html();
         //let ongoing = doc.select(".mb-3.d-flex.flex-wrap.gap-2.align-items-center span").text()
         return Response.success({
             name: doc.select("h2").text(),
